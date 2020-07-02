a855ee027a602b9e2a02ad1dc6480aaabd1706a4
nekilc <nieaowei@gmail.com>
2020-07-03 01:38:40 +0800

    add move module.

--- include/move.h
+++ include/move.h
@@ -1,11 +1,16 @@
 #ifndef __MOVE_H
 #define __MOVE_H
 
-typedef enum{
-    LEFT_TO_RIGHT=0,
-    RIGHT_TO_LEFT,
-    TOP_TO_BOTTOM,
-    BOTTOM_TO_TOP
-}MoveType;
+#include "setting.h"
+#include <pthread.h>
+
+
+typedef struct
+{
+    pthread_t id;
+}MoveInfo, *MoveThread;
+
+
+MoveThread createMoveThread(void *run(),GameSetting g);
 
 #endif
